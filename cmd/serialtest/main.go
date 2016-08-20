c36da30e97f36facdc44d8db6d121d04a9045a89
Kamil Rezac <krezac@gmail.com>
2016-08-20 22:22:44 +0200

    serial test in go, vectornav message description

---
+++ cmd/serialtest/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+        "log"
+"bufio"
+"fmt"
+
+        "github.com/tarm/serial"
+)
+
+func main() {
+        c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+        s, err := serial.OpenPort(c)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        //n, err := s.Write([]byte("test"))
+        //if err != nil {
+        //        log.Fatal(err)
+        //}
+for ;; {
+reader := bufio.NewReader(s)
+reply, err := reader.ReadBytes('\x0a')
+if err != nil {
+    panic(err)
+}
+fmt.Printf("%q\n", reply)
+
+ //       buf := make([]byte, 128)
+ //       n, err := s.Read(buf)
+ //       if err != nil {
+ //               log.Fatal(err)
+ //       }
+ //       log.Printf("%q", buf[:n])
+}
+}
---
+++ vectornav/README.md
@@ -0,0 +1,16 @@
+"$VNYMR,+104.977,+004.548,-001.276,-00.8012,-02.7376,+01.0070,+00.837,+00.235,-10.414,-00.002081,-00.001151,+00.002113*61\r\n"
+
+
+Field # Meaning Units Printf/Scanf format
+1 Yaw Angle degrees %+07.2f
+2 Pitch Angle degrees %+07.2f
+3 Roll Angle degrees %+07.2f
+4 X-Axis Magnetic N/A %+07.4f
+5 Y-Axis Magnetic N/A %+07.4f
+6 Z-Axis Magnetic N/A %+07.4f
+7 X-Axis Acceleration m/s^2 %+07.3f
+8 Y-Axis Acceleration m/s^2 %+07.3f
+9 Z-Axis Acceleration m/s^2 %+07.3f
+10 X-Axis Angular Rate rad/s %+08.4f
+11 Y-Axis Angular Rate rad/s %+08.4f
+12 Z-Axis Angular Rate rad/s %+08.4f
